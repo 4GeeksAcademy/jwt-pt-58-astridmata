bcb9adbfc1d434acf7b5c494f3980722c0e1f2f5
Astrid Mata <86080960+AstridMata@users.noreply.github.com>
2026-01-04 13:05:27 -0400

    Refactor Navbar component with logout functionality

--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,19 +1,52 @@
-import { Link } from "react-router-dom";
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import useGlobalReducer from "../hooks/useGlobalReducer";
 
 export const Navbar = () => {
+    const { store, dispatch } = useGlobalReducer();
+    const navigate = useNavigate();
+    const token = localStorage.getItem("token");
 
-	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/signup">
-						<button className="btn btn-primary">Signup</button>
-					</Link>
-				</div>
-			</div>
-		</nav>
-	);
+    const handleLogout = () => {
+        dispatch({ type: "logout" });
+        navigate("/");
+    };
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">MyApp</Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        {token && (
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/users">Users</Link>
+                            </li>
+                        )}
+                    </ul>
+
+                    <ul className="navbar-nav ms-auto">
+                        {token ? (
+                            <li className="nav-item">
+                                <button className="btn btn-outline-light" onClick={handleLogout}>Logout</button>
+                            </li>
+                        ) : (
+                            <>
+                                <li className="nav-item">
+                                    <Link className="btn btn-outline-light me-2" to="/login">Login</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="btn btn-primary" to="/signup">Signup</Link>
+                                </li>
+                            </>
+                        )}
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
 };
